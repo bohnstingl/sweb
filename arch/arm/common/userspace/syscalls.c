28d7e99216cdf4e1d3dd956f02e059854d724ac6
Thomas Bohnstingl <bohnstingl@student.tugraz.at>
2015-11-28 16:45:38 +0100

    Fixed syscall issue with more than 4 arguments

--- arch/arm/common/userspace/syscalls.c
+++ arch/arm/common/userspace/syscalls.c
@@ -2,11 +2,13 @@
 
 void __attribute__((naked)) __syscall()
 {
-  asm("push {r4,r5}\n"
-      "ldr r4, [fp, #-12]\n"
-      "ldr r5, [fp, #-8]\n"
+  asm("ldr r4, [sp, #0]\n"
+      "ldr r5, [sp, #4]\n"
+      "push {r6,r7}\n"
+      "ldr r6, [fp, #-12]\n"
+      "ldr r7, [fp, #-8]\n"
       "svc 0\n"
-      "pop {r4,r5}\n"
+      "pop {r6,r7}\n"
       "bx lr");
 }
 
